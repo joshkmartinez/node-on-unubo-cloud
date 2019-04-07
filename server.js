c029d4858c6510c89e62d05ff2ec8e6b81943fe7
Joshua Martinez <joshkmartinez@gmail.com>
2019-04-07 13:55:43 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
